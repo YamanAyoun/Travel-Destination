3499ade95a0b256e8109cfae6c0a3a22acdff152
YamanAyoun <yamanayoun0@gmail.com>
2023-04-19 03:12:26 +0000

    use the react

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css'
+function Footer() {
+  return (
+    <footer>
+      <p>Contuct Us on social media:</p>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header() {
+  return (
+    <header>
+      <h1>Hello the react web</h1>
+    </header>
+  );
+}
+
+export default Header;
